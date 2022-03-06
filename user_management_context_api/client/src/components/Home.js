c071d8ab2cb28f22e69446dec05ef98522595c27
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2022-03-06 19:04:59 +0200

    changes

--- user_management_context_api/client/src/components/Home.js
+++ user_management_context_api/client/src/components/Home.js
@@ -1,20 +1,58 @@
-import React,{useContext} from 'react'
+import React,{useContext,useState} from 'react'
 import {GlobalContext} from "../context/GlobalState"
-import UserList from './UserList';
+import {Link} from "react-router-dom";
 
 
 function Home() {
-  const {users}=useContext(GlobalContext);
+  const {users,removeUser }=useContext(GlobalContext);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [search,setSearch]=useState("")
 
   if(users.length===0) {
     return <h3 style={{textAlign:"center", margin:"90px"}}>No user has been added yet.</h3>
   }
+
+
   return (
-   users.map((user)=>{
-     return(
-       <UserList key={user.id} {...user}/>
-     )
-   })
+
+  <div style= {{marginTop:"150px"}}>
+
+
+    <table className="styled-table">
+      <thead>
+        <tr>
+        
+          <th style= {{textAlign:"center"}}>First Name</th>
+          <th style= {{textAlign:"center"}}>Last Name</th>
+          <th style= {{textAlign:"center"}}>Email</th>
+          <th style= {{textAlign:"center"}}>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user)=> {
+          return (
+            <tr key={user.id}>
+            
+            <td>{user.name}</td>
+            <td>{user.lastName}</td>
+            <td>{user.email}</td>
+            <td>
+              <Link to= {`/edit/${user.id}`}>
+
+                <button className="btn btn-edit" >
+                  Edit
+                </button>
+              </Link>
+              <button className="btn btn-delete" onClick={()=>removeUser(user.id)} >Delete</button>
+            </td>
+          </tr>
+          )
+        })}  
+      </tbody>
+    </table>
+  
+  </div>
+
   )
 }
 
